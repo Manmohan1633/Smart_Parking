4c6443f0d76bcdb56d8f2705c52b5f8292119ea5
Manmohan1633 <manmohansingh016337@gmail.com>
2024-12-03 20:25:53 +0530

    fronted and backend are both running

--- index.js
+++ index.js
@@ -105,6 +105,9 @@ app.use(cors({ origin: '*' }))
 app.use(bodyParser.json())
 app.use('/', apis);
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Parking System API!');
+});
 
 let wsConnections = []; // Array to store WebSocket connections
 
@@ -212,7 +215,9 @@ app.post('/updateParking', async (req, res) => {
 ////////////////////////////////////////////////////////////////
 
 const ip = "localhost";
-app.listen(PORT, ip, () => console.log('Server has been started'));
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 
 module.exports.wsConnections = wsConnections;
